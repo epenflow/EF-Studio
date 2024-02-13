7c12dfb3953a59f45ef9a9c29aac85494b90da59
epenflow <epenflow@gmail.com>
2024-02-14 00:49:10 +0800

    refactor terminal

--- src/app/components/Terminal/TerminalHeaderButton.tsx
+++ src/app/components/Terminal/TerminalHeaderButton.tsx
@@ -1,16 +1,22 @@
+'use client';
 import React from 'react';
 import { Button } from './libs';
 import { terminalHeaderButtonClass } from './libs/classTypes';
 import Mapping from '@/app/utils/Mapping';
 import cn from '@/app/utils/cn';
 import { buttons } from './libs/constants';
+import { useTerminalProvider } from '@/app/utils/context/TerminalContext';
 const TerminalHeaderButton = () => {
 	const handleClick = Button();
+	const { minimize } = useTerminalProvider();
 	return (
 		<div
 			id='terminal__button'
 			title='terminal__button'
-			className='flex flex-row gap-1 absolute bg-default-300'>
+			className={cn(
+				'flex flex-row gap-1 bg-default-300',
+				minimize && 'absolute'
+			)}>
 			<Mapping
 				of={buttons}
 				render={(item, index) => (
