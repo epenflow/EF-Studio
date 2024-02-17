ca85200c6fc62b289ed3f6ad6f8dd6b73fecaff4
epenflow <epenflow@gmail.com>
2024-02-17 21:09:37 +0800

    DnD components

--- src/components/Caution.tsx
+++ src/components/Caution.tsx
@@ -1,25 +1,29 @@
+'use client';
 import React from 'react';
+import Draggable from 'react-draggable';
 
 const Caution = () => {
 	return (
-		<div className='bg-[#F7D417] text-3xl lg:text-5xl font-bold uppercase text-center p-2 flex flex-col'>
-			<h1 className='text-5xl lg:text-6xl bg-black text-[#F7D417]'>
-				caution
-			</h1>
-			<span>
-				construction
-				<br />
-				area
-				<br />
-				authorized
-				<br />
-				personel only
-			</span>
-			<span className='text-xs'>
-				this new site is undergoing construction,
-				<br /> please be careful if there is suspicious activity
-			</span>
-		</div>
+		<Draggable>
+			<div className='bg-[#F7D417] text-3xl lg:text-5xl font-bold uppercase text-center p-2 flex flex-col cursor-move'>
+				<h1 className='text-5xl lg:text-6xl bg-black text-[#F7D417]'>
+					caution
+				</h1>
+				<span>
+					construction
+					<br />
+					area
+					<br />
+					authorized
+					<br />
+					personel only
+				</span>
+				<span className='text-xs'>
+					this new site is undergoing construction,
+					<br /> please be careful if there is suspicious activity
+				</span>
+			</div>
+		</Draggable>
 	);
 };
 
